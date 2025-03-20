d32b5d50170cec55c053e3eeedcb68dad664b1d7
Moritz Huber <moritz@Moritzs-MacBook-Pro.local>
2025-03-20 17:37:22 +0100

    mouse and dpi basics

--- src/geowt_002.c
+++ src/geowt_002.c
@@ -7,13 +7,18 @@
  */
 
 #define TEXTURE_SIZE 150
+/* #define WINDOW_WIDTH 3024 */
+/* #define WINDOW_HEIGHT 1964 */
+
 #define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
+#define WINDOW_WIDTH_PIXELS WINDOW_WIDTH*2
+#define WINDOW_HEIGHT 480 
+#define WINDOW_HEIGHT_PIXELS WINDOW_HEIGHT*2 
 
 #define OBJECTS_MAX 1000
 #define TEXT_BUF_MAX 1024
 
-#define DEBUG
+/* #define DEBUG */
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -37,10 +42,12 @@ enum mode_list {
 uint8_t mode = NORMAL;
 
 typedef struct {
-  int mouse_x;
-  int mouse_y;
+  double mouse_x;
+  double mouse_y;
   bool mouse_left_down;
   bool mouse_right_down;
+
+	double density;
 
   bool text_input;
   char text[TEXT_BUF_MAX];
@@ -82,17 +89,25 @@ int main() {
   Objects objects;
   objects_init(&objects);
 
+
+
   // for testing draw line not in loop but on key press, print all vars also
   // generally for testing purposes maybe ifdef testing some printf's
   // and have the poissibillity to run all drawing for all objects only once
   // on keypress
 
   while (keep_running) {
+
+
+
     keyStates = SDL_GetKeyboardState(numkeys);
     process_event(&appstate, &objects);
     objects_create(&appstate, &objects);
 #ifndef DEBUG
     draw(&appstate, &objects);
+		/* float x,y; */
+		/* SDL_GetGlobalMouseState(&x, &y); */
+		/* printf("glob mouse pos: %f, %f\n", x,y); */
 #endif
     SDL_DelayNS(1000);
   }
@@ -108,15 +123,16 @@ int sdl_init() {
   }
 
   if (!SDL_CreateWindowAndRenderer("examples/renderer/streaming-textures",
-                                   WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window,
+                                   WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_MOUSE_CAPTURE, &window,
                                    &renderer)) {
     SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
+	// texture create with pixels and not window size -> retina display scaling
   texture_window = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                                     SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH,
-                                     WINDOW_HEIGHT);
+                                     SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH_PIXELS,
+                                     WINDOW_HEIGHT_PIXELS);
   if (!texture_window) {
     SDL_Log("Couldn't create streaming texture: %s", SDL_GetError());
     return SDL_APP_FAILURE;
@@ -129,6 +145,7 @@ int appstate_init(Appstate *appstate) {
   appstate->mouse_y = 0;
   appstate->mouse_left_down = 0;
   appstate->mouse_right_down = 0;
+  appstate->density = SDL_GetWindowPixelDensity(window);
   appstate->text_input = false;
   for (int i = 0; i < TEXT_BUF_MAX; i++) {
     appstate->text[i] = 0;
@@ -173,6 +190,20 @@ void process_event(Appstate *appstate, Objects *objects) {
     case SDL_EVENT_KEY_DOWN:
 	  if (appstate->text_input) {
         if (event.key.key == SDLK_ESCAPE || event.key.key == SDLK_RETURN) {
+					if (mode == LINE) {
+						double* p0x = &objects->l2d->p1.x;
+						double* p0y = &objects->l2d->p1.y;
+						double* p1x = &objects->l2d->p2.x;
+						double* p1y = &objects->l2d->p2.y;
+						int index = 0;
+						sscanf(appstate->text, "%d:(%lf,%lf)(%lf,%lf)", &index,
+								&objects->l2d[index].p1.x, &objects->l2d[index].p1.y,
+								&objects->l2d[index].p2.x, &objects->l2d[index].p2.y);
+						char* c = appstate->text;
+						for (int i = 0; *c != '\0' && i < TEXT_BUF_MAX; i++) {
+							
+						}
+					}
           printf("\ntext input stop\n");
           appstate->text_input = false;
           SDL_StopTextInput(window);
@@ -180,8 +211,15 @@ void process_event(Appstate *appstate, Objects *objects) {
 	  } else {
         switch (event.key.key) {
         case SDLK_T:
-          printf("text input:\n");
 					appstate->text_input = true;
+					for (int i = 0; i < TEXT_BUF_MAX; i++) {
+						appstate->text[i] = 0;
+					}
+					if (mode == LINE) {
+						printf("line:(p0x,p0y)(p1x,p1y):\n");
+					} else {
+						printf("text input:\n");
+					}
           SDL_StartTextInput(window);
           break;
         case SDLK_Y:
@@ -205,18 +243,36 @@ void process_event(Appstate *appstate, Objects *objects) {
         case SDLK_D:
           if (!event.key.repeat) {
             printf("mode: %d\n", mode);
-            printf("mouse x,y: %d,%d\n", appstate->mouse_x, appstate->mouse_y);
+            printf("mouse x,y: %f,%f\n", appstate->mouse_x, appstate->mouse_y);
             printf("mouse down: %d\n", appstate->mouse_left_down);
             printf("p1 x,y: %f,%f\n", objects->p2d[0].x, objects->p2d[0].y);
             printf("p2 x,y: %f,%f\n", objects->p2d[1].x, objects->p2d[1].y);
             printf("l1: (%f,%f/%f,%f)\n", objects->l2d[0].p1.x,
                    objects->l2d[0].p1.y, objects->l2d[0].p2.x,
                    objects->l2d[0].p2.y);
+						printf("pix dens: %f\n", SDL_GetWindowPixelDensity(window));
+						int w,h;
+						SDL_GetWindowSizeInPixels(window, &w, &h);
+						printf("displayscale %f\n", SDL_GetWindowDisplayScale(window));
+						printf("size in pix: %d,%d\n", w, h);
+						SDL_GetCurrentRenderOutputSize(renderer, &w, &h);
+						printf("output size w,h: %d,%d\n", w,h);
+						float x,y;
+						SDL_GetGlobalMouseState(&x, &y);
+						printf("glob mouse pos: %f, %f\n", x,y);
           }
           break;
         case SDLK_R:
           draw(appstate, objects);
           break;
+				case SDLK_H:
+					SDL_SetWindowMouseGrab(window, 1);
+					SDL_HideCursor();
+					break;
+				case SDLK_J:
+					SDL_SetWindowMouseGrab(window, 0);
+					SDL_ShowCursor();
+					break;
         case SDLK_C:
           if (objects->l2d_index > 0) {
             objects->l2d_index--;
@@ -234,8 +290,8 @@ void process_event(Appstate *appstate, Objects *objects) {
       }
       break;
     case SDL_EVENT_MOUSE_MOTION:
-      appstate->mouse_x = event.motion.x;
-      appstate->mouse_y = event.motion.y;
+      appstate->mouse_x = SDL_lround(event.motion.x * appstate->density);
+      appstate->mouse_y = SDL_lround(event.motion.y * appstate->density);
       break;
     case SDL_EVENT_MOUSE_BUTTON_DOWN:
       appstate->mouse_left_down = event.button.down;
@@ -288,16 +344,55 @@ int objects_create(Appstate *appstate, Objects *objects) {
 
 void draw(Appstate *appstate, Objects *objects) {
   SDL_FRect dst_rect;
+  dst_rect.x = 0.0;
+  dst_rect.y = 0.0;
+  dst_rect.w = WINDOW_WIDTH_PIXELS;
+  dst_rect.h = WINDOW_HEIGHT_PIXELS;
+  SDL_FRect src_rect;
+  src_rect.x = 0.0;
+  src_rect.y = 0.0;
+  src_rect.w = WINDOW_WIDTH_PIXELS;
+  src_rect.h = WINDOW_HEIGHT_PIXELS;
+
   void *pixels;
   int pitch;
 
+
+	/* SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); */
   SDL_RenderClear(renderer); /* start with a blank canvas. */
   if (SDL_LockTexture(texture_window, NULL, &pixels, &pitch)) {
+		/* printf("pitch: %d\n", pitch); */
     uint32_t *pixs = pixels;
-    if (pix_cnt == (WINDOW_WIDTH * WINDOW_HEIGHT)) {
-      pix_cnt = 0;
-    }
-    /* pixs[pix_cnt++] = 0xFF0000FF; */
+		for (int i = 0; i < WINDOW_WIDTH_PIXELS*WINDOW_HEIGHT_PIXELS; i++) {
+			pixs[i] = 0x000000FF;
+		}
+		/*   if (pix_cnt == (WINDOW_WIDTH_PIXELS * WINDOW_HEIGHT_PIXELS)) { */
+		/*     pix_cnt = 0; */
+		/*   } */
+		/* pix_cnt+=50; */
+		/* if (pix_cnt%10 == 0) { */
+		/* 	pixs[pix_cnt] = 0xFF0000FF; */
+		/* } */
+
+		int ix = appstate->mouse_x;
+		int iy = appstate->mouse_y;
+		printf("mouse: %d, %d\n", ix, iy);
+		for (int x = ix; x < ix+5; x++) {
+			if (x <= WINDOW_WIDTH_PIXELS) {
+				pixs[x+WINDOW_WIDTH_PIXELS*iy] = 0x00FF00FF;
+			}
+			if (x-5 >= 0) {
+				pixs[x-5+WINDOW_WIDTH_PIXELS*iy] = 0x00FF00FF;
+			}
+		}
+		for (int y = iy; y < iy+5; y++) {
+			if (y <= WINDOW_HEIGHT) {
+				pixs[ix+WINDOW_WIDTH_PIXELS*y] = 0x00FF00FF;
+			}
+			if (y-5 >= 0) {
+				pixs[ix+WINDOW_WIDTH_PIXELS*(y-5)] = 0x00FF00FF;
+			}
+		}
 
     for (int l2d_cnt = 0; l2d_cnt < objects->l2d_index; l2d_cnt++) {
       int x0 = SDL_lround(objects->l2d->p1.x);
@@ -333,14 +428,9 @@ void draw(Appstate *appstate, Objects *objects) {
     SDL_UnlockTexture(texture_window); /* upload the changes (and frees the
                                           temporary surface)! */
   }
-  /* Center this one. It'll draw the latest version of the texture we drew while
-   * it was locked. */
-  dst_rect.x = 0.0;
-  dst_rect.y = 0.0;
-  dst_rect.w = (float)WINDOW_WIDTH;
-  dst_rect.h = (float)WINDOW_HEIGHT;
-  SDL_RenderTexture(renderer, texture_window, NULL, &dst_rect);
-  SDL_RenderPresent(renderer); /* put it all on the screen! */
+
+  SDL_RenderTexture(renderer, texture_window, &src_rect, &dst_rect);
+  SDL_RenderPresent(renderer);
 }
 
 void graphics(Objects *objects) {}
